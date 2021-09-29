cb971b6fcf745c85fd442b3afc8c44445eeaece9
Rajkumar Gangadharan <rajkumar.gangadharan@gmail.com>
2021-09-29 09:10:20 -0700

    test

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function initWindow()
+{
+    //window.moveTo(0,window.screen.width);
+    //window.open(location.href, "detab", "toolbar=0,status=no"); //location.href = current page
+}
+
 function moveMe(currentWidth)
 {
     window.moveTo((window.screen.width) - (currentWidth/2),0);
@@ -5,28 +11,32 @@ function moveMe(currentWidth)
 }
 
 function callListShowNative() {
-    window.innerHeight = 800;
-    window.innerWidth = 590;                              // Resizes the new window
-    window.focus();
+    window.resizeTo(590,800);
+    //window.innerHeight = 800;
+    //window.innerWidth = 590;                              // Resizes the new window
+    //window.focus();
     
 }
 
 function callListHideNative() {
-    window.innerHeight = 800;
-    window.innerWidth = 90;                              // Resizes the new window
-    window.focus();
+    window.resizeTo(100,800);
+    //window.innerHeight = 800;
+    //window.innerWidth = 90;                              // Resizes the new window
+    //window.focus();
 }
 
 function callDetailShowNative() {
-    window.innerHeight = 800;
-    window.innerWidth = 1290;                              // Resizes the new window
-    window.focus();
+    window.resizeTo(1290,800);
+    //window.innerHeight = 800;
+    //window.innerWidth = 1290;                              // Resizes the new window
+    //window.focus();
 }
 
 function callDetailHideNative() {
-    window.innerHeight = 800;
-    window.innerWidth = 590;                              // Resizes the new window
-    window.focus();
+    window.resizeTo(590,800);
+    // window.innerHeight = 800;
+    // window.innerWidth = 590;                              // Resizes the new window
+    // window.focus();
     
 }
 
