62b4d55b69d09d42768b351c9c57592e63d6a4fb
Rajkumar Gangadharan <rajkumar.gangadharan@gmail.com>
2021-09-29 00:13:04 -0700

    modified

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 function moveMe()
 {
-    window.moveTo(window.screen.width - 100,0);
+    window.moveTo(window.screen.width,0);
     window.focus();
 }
 
 function callListShowNative() {
     window.outerHeight = 800;
     window.outerWidth = 590;                              // Resizes the new window
-    window.focus(); 
+    moveMe();
     
 }
 
 function callListHideNative() {
     window.outerHeight = 800;
     window.outerWidth = 90;                              // Resizes the new window
-    window.focus();
+    moveMe();
 }
 
 function callDetailShowNative() {
     window.outerHeight = 800;
     window.outerWidth = 1290;                              // Resizes the new window
-    window.focus();
+    moveMe();
 }
 
 function callDetailHideNative() {
     window.outerHeight = 800;
     window.outerWidth = 590;                              // Resizes the new window
-    window.focus(); 
+    moveMe();
     
 }
 
